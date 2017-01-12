89c36cbc24e0d8334bdeba425513787e785af010
gajendra93 <gajendrasp.rj.9693@gmail.com>
2017-01-12 14:42:12 +0530

    Completed all the API's

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 var express = require('express');
 var app = express();
 var route = require('./route');
+var db = require('./db')
 
 app.use('/', route);
 
-app.listen(3000, function() {
-	console.log('Server started: Listening at port 3000')
-})
+db.connect('mongodb://127.0.0.1:27017/hackathon', function(err) {
+	if (err) {
+		console.log('Unable to connect to Mongo')
+	}
+	else {
+		console.log("Connected to Database");
+		app.listen(3000, function() {
+			console.log('Server started: Listening at port 3000')
+		})
+	}
+});
 
 module.exports = app;
--- route.js
+++ route.js
@@ -1,23 +1,62 @@
 var express = require('express');
-var operations = require('./operations');
+var db = require('./db')
 
 var route = express.Router();
 
 route.get('/employee', function(req, res) {
-	res.send('Employee');
+	db.get().collection('employee').find().toArray(function(err, result) {
+		res.send(result)
+	});
 })
 
 route.get('/department', function(req, res) {
-	res.send('Department');
+	db.get().collection('department').find().toArray(function(err, result) {
+		res.send(result)
+	});
 })
 
-route.get('/employee/name/:name', function(req, res) {
-	var data = operations.getEmployee(req.params.name);
-	res.send(data);
+route.get('/employee/:name', function(req, res) {
+	let obj = {}
+	db.get().collection('employee').find({"name": req.params.name}).toArray(function(err, result) {
+		if (err) {
+        	console.log(err);
+      	} else if (result.length) {
+      		obj.name = result[0].name;
+      		obj.age = result[0].age;
+      	} else {
+        	console.log('No document(s) found with defined "find" criteria!');
+      	}
+	});
+	db.get().collection('department').find({"name": req.params.name}).toArray(function(err, result) {
+		if (err) {
+        	console.log(err);
+      	} else if (result.length) {
+      		obj.deptName = result[0].deptName;
+      		res.send(obj)
+      	} else {
+        	console.log('No document(s) found with defined "find" criteria!');
+      	}
+	});
 })
 
-route.get('/storedata', function(req, res) {
-	res.send('store');
+route.post('/storedata/:name/:age/:deptName', function(req, res) {
+	let name = req.params.name;
+	let age = req.params.age;
+	let deptName = req.params.deptName;
+
+	db.get().collection('employee').insert({"name": name, "age": age}, function(err, result) {
+		if(err)
+			res.send('Failed to insert data in employee')
+		else {
+			db.get().collection('department').insert({"name": name, "deptName": deptName}, function(err, result) {
+				if(err)
+					res.send('Failed to insert data in department')
+				else {
+					res.send('Data stored');
+				}
+			});
+		}
+	});
 })
 
 module.exports = route;
